fd1453ae87d435607fceb66a073a9a97b391175c
Cassiano Menezes <cassiano.menezes.ext@panonline.com.br>
2020-12-28 11:14:42 -0300

    Ordering refactoring

--- app/src/main/java/com/cassianomenezes/restaurantsapp/database/RestaurantRepositoryImpl.kt
+++ app/src/main/java/com/cassianomenezes/restaurantsapp/database/RestaurantRepositoryImpl.kt
@@ -28,13 +28,13 @@ class RestaurantRepositoryImpl(private val restaurantDao: RestaurantDao): Restau
     }
 
     override suspend fun getAddedRestaurants(restaurants: List<Restaurant>) : DataResult<List<Restaurant>> {
-         val myRestaurants = getOriginalOrder(restaurants)
         return try {
-            myRestaurants.forEach {
+            restaurants.forEach {
                 if (restaurantDao.findByTitle(it.name) != null) {
                     it.added = true
                 }
             }
+            val myRestaurants = getOriginalOrder(restaurants)
             DataResult(RequestStatus.SUCCESS, myRestaurants)
         } catch (e:Exception) {
             print("Error: $e")
@@ -45,10 +45,17 @@ class RestaurantRepositoryImpl(private val restaurantDao: RestaurantDao): Restau
     private fun getOriginalOrder(restaurants: List<Restaurant>): List<Restaurant> {
         val allList = arrayListOf<Restaurant>()
         restaurants.run {
-            val openList = this.filter { it.status == OPEN.status }
-            val closedList = this.filter { it.status == CLOSED.status }
-            val orderAhead = this.filter { it.status == ORDER_AHEAD.status }
+            val favOpenList = this.filter { it.status == OPEN.status && it.added }
+            val favOrderAheadList = this.filter { it.status == ORDER_AHEAD.status && it.added }
+            val favClosedList = this.filter { it.status == CLOSED.status && it.added }
+
+            val openList = this.filter { it.status == OPEN.status && !it.added}
+            val closedList = this.filter { it.status == CLOSED.status && !it.added}
+            val orderAhead = this.filter { it.status == ORDER_AHEAD.status && !it.added }
             allList.run {
+                addAll(favOpenList)
+                addAll(favOrderAheadList)
+                addAll(favClosedList)
                 addAll(openList)
                 addAll(orderAhead)
                 addAll(closedList)
